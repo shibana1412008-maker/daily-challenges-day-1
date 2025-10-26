2d790c50994e5b35325324c469b048b54bf127ef
shibana1412008-maker <shibana14.1.2008@gmail.com>
2025-10-26 15:45:29 +0530

    Add files via upload

---
+++ q.12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int m1=85;
+	int m2=89;
+	int m3=91;
+	int m4=88;
+	int m5=95;
+	printf("marks in subject 1:%d\n",m1);
+	printf("marks in subect 2:%d\n",m2);
+	printf("marks in subject 3:%d\n",m3);
+	printf("marks in subject 4:%d\n",m4);
+	printf("marks in subject 5:%d\n",m5);
+	return 0;
+}
